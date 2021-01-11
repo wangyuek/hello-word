c8197aaa0ee8f79e57f98e2b07bfa8022cfc2f1d
Jason Wang <wangyuek@gmail.com>
2021-01-11 23:11:41 +0800

    random greeting

--- hello/hello.go
+++ hello/hello.go
@@ -14,7 +14,7 @@ func main() {
 	log.SetFlags(0)
 
 	//request a greeting message
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Jason")
 	//if an error was returned, print it to the console and exit the program
 	if err != nil {
 		log.Fatal(err)
